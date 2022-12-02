3189030889567c629a9a7cdf7becfee3cdbf3284
Gabriel Ross <gabriel.ross@outlook.com>
2022-12-02 18:52:02 -0500

    .

--- transaction/firestore.go
+++ transaction/firestore.go
@@ -8,8 +8,6 @@ import (
 	"github.com/gabriel-ross/barter"
 	"github.com/gabriel-ross/barter/model"
 	"google.golang.org/api/iterator"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var zeroTime time.Time
@@ -17,6 +15,27 @@ var zeroTime time.Time
 func (svc *Service) create(ctx context.Context, data model.Transaction) (_ model.Transaction, err error) {
 	data.ID = svc.db.Collection("transactions").NewDoc().ID
 	_, err = svc.db.Collection("transactions").Doc(data.ID).Set(ctx, data)
+	if err != nil {
+		return model.Transaction{}, err
+	}
+
+	payments := []firestore.Update{}
+	credits := []firestore.Update{}
+	for key, val := range data.Quantities {
+		payments = append(payments, firestore.Update{
+			Path:  "funds." + key,
+			Value: firestore.Increment((-1) * val),
+		})
+		credits = append(payments, firestore.Update{
+			Path:  "funds." + key,
+			Value: firestore.Increment(val),
+		})
+	}
+	_, err = svc.db.Collection("accounts").Doc(data.SenderAccountID).Update(ctx, payments)
+	if err != nil {
+		return model.Transaction{}, err
+	}
+	_, err = svc.db.Collection("accounts").Doc(data.RecipientAccountID).Update(ctx, credits)
 	if err != nil {
 		return model.Transaction{}, err
 	}
@@ -67,17 +86,6 @@ func (svc *Service) read(ctx context.Context, id string) (_ model.Transaction, e
 	return m, nil
 }
 
-func (svc *Service) exists(ctx context.Context, id string) (_ bool, err error) {
-	_, err = svc.db.Collection("transactions").Doc(id).Get(ctx)
-	if status.Code(err) == codes.NotFound {
-		return false, nil
-	} else if err != nil {
-		return false, err
-	} else {
-		return true, nil
-	}
-}
-
 func (svc *Service) set(ctx context.Context, id string, data model.Transaction) (_ model.Transaction, err error) {
 	data.ID = id
 	_, err = svc.db.Collection("transactions").Doc(data.ID).Set(ctx, data)
@@ -124,6 +132,41 @@ func (svc *Service) updateNonZero(ctx context.Context, id string, data model.Tra
 
 func (svc *Service) delete(ctx context.Context, id string) (err error) {
 	_, err = svc.db.Collection("transactions").Doc(id).Delete(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc *Service) cascadeDelete(ctx context.Context, id string) (err error) {
+	data, err := svc.read(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	_, err = svc.db.Collection("transactions").Doc(id).Delete(ctx)
+	if err != nil {
+		return err
+	}
+
+	payments := []firestore.Update{}
+	credits := []firestore.Update{}
+	for key, val := range data.Quantities {
+		payments = append(payments, firestore.Update{
+			Path:  "funds." + key,
+			Value: firestore.Increment((-1) * val),
+		})
+		credits = append(payments, firestore.Update{
+			Path:  "funds." + key,
+			Value: firestore.Increment(val),
+		})
+	}
+	_, err = svc.db.Collection("accounts").Doc(data.SenderAccountID).Update(ctx, credits)
+	if err != nil {
+		return err
+	}
+	_, err = svc.db.Collection("accounts").Doc(data.RecipientAccountID).Update(ctx, payments)
 	if err != nil {
 		return err
 	}
